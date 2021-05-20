4bfdaa2cbf73b0ae35b7990531748ad0cef7ca0f
= <birtday11111@gmail.com>
2021-05-20 22:26:26 +0700

    new web

--- routes/courses.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const BookControllers = require('../app/controllers/BookControllers');
-router.get('/create',BookControllers.create);
-router.post('/store',BookControllers.store);
-router.put('/:id',BookControllers.update);
-router.get('/:slug',BookControllers.show);
-module.exports = router;
--- routes/home.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeControllers = require('../app/controllers/homeControllers');
-router.use('/',homeControllers.index);
-module.exports = router;
